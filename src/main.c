82e726bacb180f018777e4a6c7ec1b99ae89f76c
PolinaProkhorova <148792250+PolinaProkhorova@users.noreply.github.com>
2026-05-26 13:45:30 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h> 
 #include "stats.h"
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Использование: %s <имя_файла>\n", argv[0]);
-        return EXIT_FAILURE; // Если файл не указан, выходим с ошибкой  
+    char manual_filename[256];
+    char *filename;
+
+    if (argc == 2) {
+        filename = argv[1];
+    } else {
+        printf("Имя файла не указано при запуске.\n");
+        printf("Введите имя файла: ");
+        scanf("%255s", manual_filename);
+        filename = manual_filename;
     }
 
-    FILE *file = fopen(argv[1], "r");
+    FILE *file = fopen(filename, "r");
     if (file == NULL) {
         perror("Ошибка открытия файла");
         return EXIT_FAILURE; // Если файл не удалось открыть, выходим с ошибкой  
@@ -23,6 +31,13 @@ int main(int argc, char *argv[]) {
     printf("Строки: %d\n", lines);
     printf("Слова: %d\n", words);
     printf("Символы: %d\n", chars);
+
+    // Открываем файл заново для функции частоты слов
+    file = fopen(filename, "r");
+    if (file != NULL) {
+        count_word_frequency(file);
+        fclose(file);
+    }
 
     return EXIT_SUCCESS;
 }
