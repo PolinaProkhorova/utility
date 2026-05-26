9bc07fdff824b492e62f18b3b19ca35c935f8c7b
PolinaProkhorova <148792250+PolinaProkhorova@users.noreply.github.com>
2026-05-26 13:42:10 +0300

    Update stats.h

--- include/stats.h
+++ include/stats.h
@@ -1,7 +1,9 @@
 #ifndef STATS_H  
 #define STATS_H
+#include <stdio.h>
 
 // Объявление функций для подсчета  
 void count_stats(FILE *file, int *lines, int *words, int *chars);
+void count_word_frequency(FILE *file);
 
 #endif
